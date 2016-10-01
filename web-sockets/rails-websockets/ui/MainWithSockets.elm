269519b4397b697501d89af2f4c80670c54b956a
Jacob Oakes <jacoboakes92@gmail.com>
2016-10-01 11:37:56 -0400

    Can not connect to channels

--- web-sockets/rails-websockets/ui/MainWithSockets.elm
+++ web-sockets/rails-websockets/ui/MainWithSockets.elm
@@ -4,7 +4,8 @@ import Html.App
 import Model exposing (..)
 import View exposing (..)
 import WebSocket
-import Json.Encode as Json
+import Json.Encode
+import Json.Decode exposing (andThen, (:=))
 
 
 type Msg
@@ -20,15 +21,32 @@ init =
     , allMsgs = []
     , personalMsgs = []
     }
-        ! [ WebSocket.send "ws://localhost:3000/cable" <|
-                Json.encode 0 <|
-                    Json.object
-                        [ ( "command", Json.string "subscribe" )
-                        , ( "identifier"
-                          , Json.string "{\"channel\":\"AllMessagesChannel\"}"
-                          )
-                        ]
+        ! [ connectToChannel "AllMessagesChannel"
+          , connectToChannel "PersonalMessagesChannel"
           ]
+
+
+connectToChannel : String -> Cmd Msg
+connectToChannel name =
+    WebSocket.send socketUrl <|
+        Json.Encode.encode 0 <|
+            Json.Encode.object
+                [ ( "command", Json.Encode.string "subscribe" )
+                , ( "identifier"
+                  , Json.Encode.string <| "{\"channel\":\"" ++ name ++ "\"}"
+                  )
+                ]
+
+
+socketUrl : String
+socketUrl =
+    "ws://localhost:3000/cable"
+
+
+type SocketMsg
+    = Ping
+    | ConnectedToChannel String
+    | Unknown
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -37,9 +55,45 @@ update msg model =
         ReceivedMessage message ->
             let
                 _ =
-                    Debug.log "recieved message" message
+                    Debug.log "raw message" message
+
+                decodeMsg msgType =
+                    case msgType of
+                        "ping" ->
+                            Json.Decode.succeed Ping
+
+                        "confirm_subscription" ->
+                            Json.Decode.object1 ConnectedToChannel <|
+                                Json.Decode.customDecoder ("identifier" := Json.Decode.string) <|
+                                    Json.Decode.decodeString ("channel" := Json.Decode.string)
+
+                        _ ->
+                            Json.Decode.succeed Unknown
+
+                decoder =
+                    ("type" := Json.Decode.string) `andThen` decodeMsg
+
+                decodedMsg =
+                    Result.withDefault Unknown <|
+                        Json.Decode.decodeString decoder message
+
+                _ =
+                    Debug.log "decoded message" decodedMsg
             in
-                model ! []
+                case decodedMsg of
+                    ConnectedToChannel channel ->
+                        case channel of
+                            "AllMessagesChannel" ->
+                                { model | allStatus = Connected } ! []
+
+                            "PersonalMessagesChannel" ->
+                                { model | personalStatus = Connected } ! []
+
+                            _ ->
+                                model ! []
+
+                    _ ->
+                        model ! []
 
         SendMsgToAll ->
             model ! []
@@ -50,7 +104,7 @@ update msg model =
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    WebSocket.listen "ws://localhost:3000/cable" ReceivedMessage
+    WebSocket.listen socketUrl ReceivedMessage
 
 
 main : Program Never
